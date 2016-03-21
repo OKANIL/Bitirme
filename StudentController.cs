9d72a4871d42aaa684c1e1b4d4d318e81cdc22f9
OKAN TİKTAŞ <okantiktas@gmail.com>
2016-03-21 20:23:28 +0200

    Create StudentController.cs

---
+++ StudentController.cs
@@ -0,0 +1,39 @@
+public class StudentController : ApiController
+ {
+    //Burada öğrencilerimiz üzerine kayıtlar giriyoruz
+    //ve bunları ekranda göstereceğiz
+    Student[] students = new Student[]
+    {
+      new Student { StudentNo = 1 },
+      new student { studentNo = 2 }
+      
+  };
+ 
+   // GET api/
+   //Bu metod ile tüm student çekilecektir
+     public IEnumerable<student> Get_Tumstudent()
+     {
+        return student;
+     }
+ 
+   // GET api//3
+   //Örneğin 3 yazarak, studentNo'su 3 olan kişi çekilecektir.
+     public student Get_studentNoileGetir(int id)
+     {
+        var student = student.FirstOrDefault((p) => p.studentNo == id);
+        if (student == null)
+        {
+          throw new HttpResponseException(HttpStatusCode.NotFound);
+        }
+          return student;
+     }
+ 
+    // GET api//Programci
+    //Örneğin Programci yazarak, mesleği programcı olanlar gelecektir.
+      public IEnumerable<student> Get_MeslegeGoreUrunGetir(string meslek)
+      {
+        return student.Where(
+        (p) => string.Equals(p.Meslek, meslek,
+        StringComparison.OrdinalIgnoreCase));
+      }
+    }
